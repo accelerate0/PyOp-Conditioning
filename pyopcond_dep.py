e0ba41c57e6d8ee68ba6c5840a996dd3eb5a9cba
Erick Won <ecw207@musc.edu>
2022-06-21 09:23:34 -0400

    Now needs bug fix

--- pyopcond_dep.py
+++ pyopcond_dep.py
@@ -2,20 +2,18 @@ import numpy as np
 import math
 import random
 
-# + ========================================= +
-# +++ Variable Interval Scheduling Generator +++
-#
-#       Utilizes the Hoffman-Fleshler Constant Probability Distribution
-#       Based on Hantula (1991) and Fleshler et Hoffman (1962)
-#       Program Parameters
-#           N = Amount of terms
-#           m = mean
-#           n = the nth term of progression
+
+
+#==========================================================#
+#       Variable Interval (VI) Scheduling Generator        #
+#==========================================================#
+# Utilizes the Hoffman-Fleshler Constant Probability Distribution
+# Based on Hantula (1991) and Fleshler et Hoffman (1962)
 
 def var_int(N, mean):
     # Declaring Variables
-    var_int.mean = mean
-    var_int.N = N
+    var_int.mean = mean # mean
+    var_int.N = N # Amount of terms
     var_int.new_array = []
     # Creating Output Array
     var_int.progression = list(range(1, var_int.N, 1))
@@ -24,26 +22,28 @@ def var_int(N, mean):
     print("Mean (m):", var_int.mean)
     print("Amount of terms (N):", var_int.N)
     print("Array values for n:", var_int.progression)
-    for var_int.n in var_int.progression:
+    for var_int.n in var_int.progression: # n = the nth term of progression
       if var_int.n == var_int.N:
         var_int.new_array_add = var_int.mean * (1 + math.log(N))
         var_int.new_array.append(var_int.new_array_add)
       else:
-        var_int.b = 1 + math.log(var_int.N, np.e) + (var_int.N - n) * math.log(var_int.N - var_int.n, np.e)
+        var_int.b = 1 + math.log(var_int.N, np.e) + (var_int.N - var_int.n) * math.log(var_int.N - var_int.n, np.e)
         var_int.c = (var_int.N - var_int.n + 1) * math.log(var_int.N - var_int.n + 1, np.e)
         var_int.new_array_add = var_int.mean * (var_int.b - var_int.c)
         var_int.new_array.append(var_int.new_array_add)
     var_int.output_random = random.shuffle(var_int.new_array)
     var_int.output_straight = var_int.new_array
 
-# + ========================================= +
-# +++ Random Interval Scheduling Generator +++
-#
-#       Based on Millenson (1963) and Bancroft et Bourrett (2008)
+
+
+#==========================================================#
+#         Random Interval (RI) Scheduling Generator        #
+#==========================================================#
+# Based on Millenson (1963) and Bancroft et Bourrett (2008)
 
 def rand_int(interval, amount): #()
     # Declaring Variables
-    rand_int.amount = 4 # Amount of intervals (sec)
+    rand_int.amount = amount # Amount of intervals (sec)
     rand_int.interval = interval # Length of each interval (sec)
     rand_int.prob = 1/rand_int.interval
     print('x')
@@ -68,10 +68,11 @@ def rand_int(interval, amount): #()
                   rand_int.new_array.append(rand_int.interval_multiplier)
     rand_int.output = rand_int.new_array
 
-# + ========================================= +
-# +++ Variable Ratio Scheduling Generator +++
-#
-#
+
+
+#==========================================================#
+#        Variable Ratio (VR) Scheduling Generator          #
+#==========================================================#
 
 def var_ratio(amount, min, max): #()
     # Declaring Variables
@@ -87,35 +88,29 @@ def var_ratio(amount, min, max): #()
         var_ratio.new_array.append(var_ratio.n)
     var_ratio.output = var_ratio.new_array
 
-# + ========================================= +
-# +++ Random Ratio Scheduling Generator +++
-#
-#
-def rand_ratio(interval, mean): #()
+
+
+#==========================================================#
+#          Random Ratio (RR) Scheduling Generator          #
+#==========================================================#
+
+def rand_ratio(mean, amount): #()
     # Declaring Variables
-    rand_ratio.mean = 4 # Amount of intervals (sec)
-    rand_ratio.interval = interval # Length of each interval (sec)
-    rand_ratio.prob = 1/rand_ratio.interval
+    rand_ratio.mean = mean # Mean ratio value
+    rand_ratio.amount = amount # Amount of inputs
+    rand_ratio.prob = 1/rand_ratio.interval # Probability calculator
     print('x')
-    # Creating Array For within RI
+    # Creating Array
     rand_ratio.new_array = []
-    rand_ratio.progression = list(range(1, rand_ratio.interval, 1))
-    rand_ratio.progression.append(rand_ratio.interval)
-    # Creating Array For between RI
-    rand_ratio.multiplier = list(range(1, rand_ratio.amount, 1))
-    rand_ratio.multiplier.append(rand_ratio.amount)
+    rand_ratio.progression = list(range(1, rand_ratio.amount, 1))
+    rand_ratio.progression.append(rand_ratio.amount)
     # Generating number
-    print("Experiment length (sec):", rand_ratio.interval)
-    print("Probability factor (prob/sec):", rand_ratio.prob)
-    for rand_ratio.multiplier in range(rand_ratio.amount):
-        for rand_ratio.n in rand_ratio.progression:
-            rand_ratio.random = np.random.uniform(0,1)
-            if rand_ratio.random <= rand_ratio.prob:
-                if rand_ratio.multiplier == 1:
-                  rand_ratio.new_array.append(rand_ratio.n)
-                else:
-                  rand_ratio.interval_multiplier = rand_ratio.n + (rand_ratio.interval * rand_ratio.multiplier)
-                  rand_ratio.new_array.append(rand_ratio.interval_multiplier)
+    print("Amount (input):", rand_ratio.amount)
+    print("Probability factor (prob/input):", rand_ratio.prob)
+    for rand_ratio.n in rand_ratio.progression:
+        rand_ratio.random = np.random.uniform(0,1)
+        if rand_ratio.random <= rand_ratio.prob:
+            rand_ratio.new_array.append(rand_ratio.n)
     rand_int.output = rand_int.new_array
 
 
--- pyopcond_standalone.py
+++ pyopcond_standalone.py
@@ -2,70 +2,121 @@ import numpy as np
 import math
 import random
 
-# + ========================================= +
-# +++ Variable Interval Scheduling Generator +++
-#
-#       Utilizes the Hoffman-Fleshler Constant Probability Distribution
-#       Based on Hantula (1991) and Fleshler et Hoffman (1962)
-#       Program Parameters
-#           N = Amount of terms
-#           m = mean
-#           n = the nth term of progression
+
+
+#==========================================================#
+#       Variable Interval (VI) Scheduling Generator        #
+#==========================================================#
+# Utilizes the Hoffman-Fleshler Constant Probability Distribution
+# Based on Hantula (1991) and Fleshler et Hoffman (1962)
 
 def var_int(N, mean):
+    # Declaring Variables
+    var_int.mean = mean # mean
+    var_int.N = N # Amount of terms
     var_int.new_array = []
-    var_int.progression = list(range(1, N, 1))
+    # Creating Output Array
+    var_int.progression = list(range(1, var_int.N, 1))
     var_int.progression.append(N)
     var_int.new_array_add = 0
-    print("Mean (m):", mean)
-    print("Amount of terms (N):", N)
+    print("Mean (m):", var_int.mean)
+    print("Amount of terms (N):", var_int.N)
     print("Array values for n:", var_int.progression)
-    for n in var_int.progression:
-      if n == N:
-        var_int.new_array_add = mean * (1 + math.log(N))
+    for var_int.n in var_int.progression: # n = the nth term of progression
+      if var_int.n == var_int.N:
+        var_int.new_array_add = var_int.mean * (1 + math.log(N))
         var_int.new_array.append(var_int.new_array_add)
       else:
-        b = 1 + math.log(N, np.e) + (N - n) * math.log(N - n, np.e)
-        c = (N - n + 1) * math.log(N - n + 1, np.e)
-        var_int.new_array_add = mean * (b - c)
+        var_int.b = 1 + math.log(var_int.N, np.e) + (var_int.N - var_int.n) * math.log(var_int.N - var_int.n, np.e)
+        var_int.c = (var_int.N - var_int.n + 1) * math.log(var_int.N - var_int.n + 1, np.e)
+        var_int.new_array_add = var_int.mean * (var_int.b - var_int.c)
         var_int.new_array.append(var_int.new_array_add)
     var_int.output_random = random.shuffle(var_int.new_array)
     var_int.output_straight = var_int.new_array
 
-# + ========================================= +
-# +++ Random Interval Scheduling Generator +++
-#
-#       Based on Millenson (1963) and Bancroft et Bourrett (2008)
 
+
+#==========================================================#
+#         Random Interval (RI) Scheduling Generator        #
+#==========================================================#
+# Based on Millenson (1963) and Bancroft et Bourrett (2008)
 
 def rand_int(interval, amount): #()
-  rand_int.amount = 4 # Amount of intervals (sec)
-  rand_int.interval = interval # Length of each interval (sec)
-  rand_int.prob = 1/rand_int.interval
-  print('x')
-  # Creating Array For within RI
-  rand_int.new_array = []
-  rand_int.progression = list(range(1, rand_int.interval, 1))
-  rand_int.progression.append(rand_int.interval)
-  # Creating Array For between RI
-  rand_int.multiplier = list(range(1, rand_int.amount, 1))
-  rand_int.multiplier.append(rand_int.amount)
-  # Generating number
-  print("Experiment length (sec):", rand_int.interval)
-  print("Probability factor (prob/sec):", rand_int.prob)
-  for rand_int.multiplier in range(rand_int.amount):
-    for n in rand_int.progression:
-        rand_int.random = np.random.uniform(0,1)
-        if rand_int.random <= rand_int.prob:
-            if rand_int.multiplier == 1:
-              rand_int.new_array.append(n)
-            else:
-              interval_multiplier = n + (rand_int.interval * rand_int.multiplier)
-              rand_int.new_array.append(interval_multiplier)
-  print("Throughout the entire RI session of", (rand_int.amount*rand_int.interval),"(sec), RI trigger generated for:", rand_int.new_array, "sec")
-  rand_int.output = rand_int.new_array
+    # Declaring Variables
+    rand_int.amount = amount # Amount of intervals (sec)
+    rand_int.interval = interval # Length of each interval (sec)
+    rand_int.prob = 1/rand_int.interval
+    print('x')
+    # Creating Array For within RI
+    rand_int.new_array = []
+    rand_int.progression = list(range(1, rand_int.interval, 1))
+    rand_int.progression.append(rand_int.interval)
+    # Creating Array For between RI
+    rand_int.multiplier = list(range(1, rand_int.amount, 1))
+    rand_int.multiplier.append(rand_int.amount)
+    # Generating number
+    print("Experiment length (sec):", rand_int.interval)
+    print("Probability factor (prob/sec):", rand_int.prob)
+    for rand_int.multiplier in range(rand_int.amount):
+        for rand_int.n in rand_int.progression:
+            rand_int.random = np.random.uniform(0,1)
+            if rand_int.random <= rand_int.prob:
+                if rand_int.multiplier == 1:
+                  rand_int.new_array.append(rand_int.n)
+                else:
+                  rand_int.interval_multiplier = rand_int.n + (rand_int.interval * rand_int.multiplier)
+                  rand_int.new_array.append(rand_int.interval_multiplier)
+    rand_int.output = rand_int.new_array
 
-# + ========================================= +
-# +++ Variable Ratio Scheduling Generator +++
-#
-#
+
+
+#==========================================================#
+#        Variable Ratio (VR) Scheduling Generator          #
+#==========================================================#
+
+def var_ratio(amount, min, max): #()
+    # Declaring Variables
+    var_ratio.amount = amount # Amount of total rewards (sec)
+    var_ratio.min = min # Minimum amount of inputs (ie lever presses)
+    var_ratio.max = max  # Maximum amount of inputs (ie lever presses)
+    # Creating Output Array
+    var_ratio.new_array = []
+    var_ratio.progression = list(range(1, var_ratio.amount, 1))
+    var_ratio.progression.append(var_ratio.amount)
+    for var_ratio.n in range(var_ratio.progression):
+        random.randrange(var_ratio.min, 1 + var_ratio.max, 1)
+        var_ratio.new_array.append(var_ratio.n)
+    var_ratio.output = var_ratio.new_array
+
+
+
+#==========================================================#
+#          Random Ratio (RR) Scheduling Generator          #
+#==========================================================#
+
+def rand_ratio(mean, amount): #()
+    # Declaring Variables
+    rand_ratio.mean = mean # Mean ratio value
+    rand_ratio.amount = amount # Amount of inputs
+    rand_ratio.prob = 1/rand_ratio.interval # Probability calculator
+    print('x')
+    # Creating Array
+    rand_ratio.new_array = []
+    rand_ratio.progression = list(range(1, rand_ratio.amount, 1))
+    rand_ratio.progression.append(rand_ratio.amount)
+    # Generating number
+    print("Amount (input):", rand_ratio.amount)
+    print("Probability factor (prob/input):", rand_ratio.prob)
+    for rand_ratio.n in rand_ratio.progression:
+        rand_ratio.random = np.random.uniform(0,1)
+        if rand_ratio.random <= rand_ratio.prob:
+            rand_ratio.new_array.append(rand_ratio.n)
+    rand_int.output = rand_int.new_array
+
+
+
+
+
+
+
+# ======= #
